02aa94c362e8dc331a52e6468b31bd0ee4b116c2
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-12 20:27:16 +0530

    Allow addition of alumni and alumni status from active admin

--- app/admin/alumni_status.rb
+++ app/admin/alumni_status.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AlumniStatus do
-
+  permit_params :Alumni_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
