9f235eaf558923adfa3d6f4ce620dd8f2bae6198
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-12 20:29:48 +0530

    Default values for the Alumni Statuses.
    After the initialize action, the init action is declared as a callback and it will 
    set the search status to tobesearched and response status to default.
    
    Also defines search and response as enums, instead of just numbers.
    
    FIXES #6

--- app/models/alumni_status.rb
+++ app/models/alumni_status.rb
@@ -1,3 +1,13 @@
 class AlumniStatus < ActiveRecord::Base
-  belongs_to :Alumni
+	belongs_to :Alumni
+	enum search: [:ready, :found, :tobesearched, :dumped]
+	enum response: [:positive, :negative, :neutral, :registered, :default]
+
+	after_initialize :init
+
+	def init
+	  self.tobesearched!
+	  self.default!
+	end
+
 end
