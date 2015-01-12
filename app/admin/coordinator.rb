5f6da4d80aca1815d4e23181ca8325b426907910
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-12 16:55:18 +0530

    Permit parameters in activeadmin "add record".

--- app/admin/coordinator.rb
+++ app/admin/coordinator.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Coordinator do
-
+  permit_params :email, :name, :rollnum, :portfolio
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
