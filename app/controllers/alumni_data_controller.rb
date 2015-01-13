6d95e77b057351bfc23e9aaf499014efff0bbba9
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-13 20:27:33 +0530

    Implement the update action.
    -  Anyone can update an alumni record now.
    
    Constrain the editing permissions to the student member who is allotted to 
    the particular alumni.

--- app/controllers/alumni_data_controller.rb
+++ app/controllers/alumni_data_controller.rb
@@ -1,4 +1,17 @@
 class AlumniDataController < ApplicationController
 	def update
+		# render plain: params.inspect
+		@alumni_data = AlumniData.find(params[:id])
+
+		if @alumni_data.update(alumni_data_params)
+			render plain: "Update has been completed."
+		else
+			render "Update could not be completed."
+		end
+	end
+
+	private
+	def alumni_data_params
+		params.require(:alumni_data).permit(AlumniData.column_names)
 	end
 end
