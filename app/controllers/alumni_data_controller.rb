76e8f079abf401fb9748b22cf9b185e0675dbb8f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-13 20:53:36 +0530

    this shows if the studmem signed in has permissions to edit the alumni.
    - Almost solved the issue #2

--- app/controllers/alumni_data_controller.rb
+++ app/controllers/alumni_data_controller.rb
@@ -1,10 +1,23 @@
 class AlumniDataController < ApplicationController
 	def update
+
 		# render plain: params.inspect
 		@alumni_data = AlumniData.find(params[:id])
 
 		if @alumni_data.update(alumni_data_params)
-			render plain: "Update has been completed."
+			# render plain: "Update has been completed."
+			if student_member_signed_in?
+				if @tie = TieAlumniWithStudentMember.find_by_StudentMember_id(current_student_member.id)
+					if params[:id].to_i == @tie.Alumni_id
+						render plain: "This studmem has permissions to edit the Alumni."
+					else
+						render plain: "This studmem does not permissions to edit the Alumni."
+					end
+				else
+					render plain: "This studmem does not permissions to edit the Alumni."
+				end
+
+			end
 		else
 			render "Update could not be completed."
 		end
