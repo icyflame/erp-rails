20feec371d5c6334c5dae5c12e86725b132e3194
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-13 20:09:18 +0530

    Add alumni_data as a route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :student_member
   resources :alumni
+  resource :alumni_datum
 
 	root 'student_member#index'
   # The priority is based upon order of creation: first created -> highest priority.
