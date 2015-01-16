0b7e1182f0db34a057806e821591a4fb9bc3fcd5
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-01-16 20:08:45 +0530

    Show the StudMem to whom the alum is assigned.
    - on the alum profile page.

--- app/controllers/alumni_controller.rb
+++ app/controllers/alumni_controller.rb
@@ -9,6 +9,10 @@ class AlumniController < ApplicationController
 
   	@alumni_data = AlumniData.find_by_Alumni_id(@thisID)
 
+    if @tie = TieAlumniWithStudentMember.find_by_StudentMember_id(current_student_member.id)
+      @assignedtoid = @tie.Alumni_id
+    end
+
   	# render plain: @alumni_data.inspect
 
   	# render plain: @astat.inspect
