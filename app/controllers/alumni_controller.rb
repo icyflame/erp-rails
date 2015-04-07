d549c98c669fd13a863ba6a5a0bdcdca4bdb5231
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-07 23:55:55 +0530

    Show the student member on the alumni profile page

--- app/controllers/alumni_controller.rb
+++ app/controllers/alumni_controller.rb
@@ -8,9 +8,14 @@ class AlumniController < ApplicationController
   	@astat = AlumniStatus.find_by_Alumni_id(@thisID)
 
   	@alumni_data = AlumniData.find_by_Alumni_id(@thisID)
+    @editingAllowed = false
+    @assignedtoid = -1
 
     if @tie = TieAlumniWithStudentMember.find_by_StudentMember_id(current_student_member.id)
       @assignedtoid = @tie.Alumni_id
+      if @assignedtoid == current_student_member.id
+        @editingAllowed = true
+      end
     end
 
   	# render plain: @alumni_data.inspect
