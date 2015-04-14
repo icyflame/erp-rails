3b0da0f3d3cfc54f8b2b9459a683f8efe23bdf38
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-14 23:59:58 +0530

    Create the basic draft of the Student Home Page.
    - This will be the central place where the student will interact with the portal.
    - It will have a sortable table, with basic columns regarding the alumni, 
    and a link to the alumni's profile.

--- app/controllers/student_member_controller.rb
+++ app/controllers/student_member_controller.rb
@@ -5,9 +5,20 @@ class StudentMemberController < ApplicationController
 		# ensure that only student member who
 		# is logged in can see his/her own
 		# record.
-  	@thisID = current_student_member.id
+    @thisID = current_student_member.id
+
+    @allties = TieAlumniWithStudentMember.where("StudentMember_id = #{current_student_member.id}")
+
+    @allalums = Array.new
+
+    @allties.each do |this_tie|
+    # puts Alumni.find(this_tie.Alumni_id).name
+    @allalums.push(Alumni.find(this_tie.Alumni_id))
   end
 
-  def show
-  end
+  # render plain: @allties.inspect
+end
+
+def show
+end
 end
