74dc2b064480b6dc39127268a6e780907fab2d82
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-18 20:27:46 +0530

    toggle serve_static_assets to true.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,15 +20,14 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
   # Generate digests for assets URLs.
   config.assets.digest = true
 
@@ -62,7 +61,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
