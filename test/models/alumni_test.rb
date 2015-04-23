6fb5bb0cb7d227f5b95687b35facd577a9b45659
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-23 15:39:37 +0530

    Add basic fixtures, and one basic test
    - Fixtures for creating 10 alumni, studmems and coords
    - One basic test for checking if the alumni were created or not.

--- test/models/alumni_test.rb
+++ test/models/alumni_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class AlumniTest < ActiveSupport::TestCase
+
+	test "number_of_records_being_created" do
+		a = Alumni.count
+		assert_equal a, 10, "The ten alumni were not created"
+	end
+
+	test "records_being_created" do
+		a = Alumni.where({ :name => "alumni1" })
+		assert_equal a, 10, "The ID is not equal"
+	end
+
+	# test "validations_of_columns" do
+	# 	alumni = Alumni.new
+	# 	alumni.name = "Something"
+	# 	assert_not alumni.save, "The alumni was saved even though there was no hall or department given."
+	# end
   # test "the truth" do
   #   assert true
   # end
