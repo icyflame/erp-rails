ce400560020f7e055ee6f2c7d64237f89670d366
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-24 20:38:25 +0530

    Complete the routing action inside routing#index.

--- app/controllers/routing_controller.rb
+++ app/controllers/routing_controller.rb
@@ -1,4 +1,13 @@
 class RoutingController < ApplicationController
 	def index
+		if student_member_signed_in?
+			redirect_to "/homepage/studentmember/"
+		else
+			if coordinator_signed_in?
+				redirect_to "/homepage/coordinator/"
+			else
+				redirect_to "/homepage/"
+			end
+		end	
 	end
 end
