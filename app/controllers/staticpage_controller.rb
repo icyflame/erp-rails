f5dde9db1a9e42bce230e06e4a2bcc3966913d68
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-25 14:23:37 +0530

    Creating a view for creating all admin users.
    - Create the users if and only if the user does not exist in the DB
      already.

--- app/controllers/staticpage_controller.rb
+++ app/controllers/staticpage_controller.rb
@@ -2,4 +2,30 @@ class StaticpageController < ApplicationController
 	def index
 		
 	end
+
+	def createallusers
+
+		AdminUser.first_or_create!(email: 'admin@example.com', 
+			password: 'password', 
+			password_confirmation: 'password')
+
+		StudentMember.first_or_create!(id: '0',
+			name: 'Default User', 
+			rollnum: '15DF12345',
+			portfolio: 'Default Portfolio',
+			email: 'student@example.com', 
+			password: 'password', 
+			password_confirmation: 'password')
+
+		Coordinator.first_or_create!(id: '0',
+			name: 'Default User',
+			rollnum: '15DF12346',
+			portfolio: 'Default Portfolio',
+			email: 'coordinator@example.com', 
+			password: 'password', 
+			password_confirmation: 'password')
+
+		render plain: "Done!"
+		
+	end
 end
