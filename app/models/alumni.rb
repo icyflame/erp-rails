82dfe24a54d968eb053a469c0a907c2d2e9b19ff
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-04-25 14:10:28 +0530

    Create all the records simultaneously
    - Creation of the alumni record will lead to the creation of the 
      alumni status and alumni data records too.

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,2 +1,13 @@
 class Alumni < ActiveRecord::Base
+	has_one :AlumniStatus
+	has_one :AlumniData
+	after_create :build_other_records
+
+	private
+	def build_other_records
+		AlumniStatus.create({ :Alumni_id => self.id })
+		AlumniData.create({ :Alumni_id => self.id })
+		TieAlumniWithStudentMember.create({ :Alumni_id => self.id, :StudentMember_id => 0})
+		true
+	end
 end
