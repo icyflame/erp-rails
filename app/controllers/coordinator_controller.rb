04456dc43422435f256cd2347488705c371bcd14
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-06 17:13:51 +0530

    Fix the rendering of the coordinator home page.
    - Shows a list of all the alumni with assigned to, and search and 
    response status.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/controllers/coordinator_controller.rb
+++ app/controllers/coordinator_controller.rb
@@ -7,11 +7,6 @@ class CoordinatorController < ApplicationController
 
     @allAlums = Alumni.all()
 
-    @allAlums.each do |alumni|
-    	@tie = TieAlumniWithStudentMember.where({ :Alumni_id => "#{alumni.id}"})
-    	# student_id = @tie[0].StudentMember_id
-    	# student = StudentMember.find(student_id)
-    end
   end
 
   def show
