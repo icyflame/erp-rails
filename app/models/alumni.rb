55718315fbdf83871b4994ec0074638d791be63a
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-06 17:13:11 +0530

    Add the before_destroy callback in Alumni model
    - Before destroying an Alumni, always destroy the associated
      AlumniStatus, AlumniData and the TieAlumni records too.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -2,6 +2,9 @@ class Alumni < ActiveRecord::Base
 	has_one :AlumniStatus
 	has_one :AlumniData
 	after_create :build_other_records
+	before_destroy { |record| AlumniStatus.destroy_all "Alumni_id = #{record.id}"   }
+	before_destroy { |record| AlumniData.destroy_all "Alumni_id = #{record.id}"   }
+	before_destroy { |record| TieAlumniWithStudentMember.destroy_all "Alumni_id = #{record.id}"   }
 
 	private
 	def build_other_records
