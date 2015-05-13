e398d2f388ba00050bff2ca482f4b1eb368d3acd
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-14 01:00:19 +0530

    Viewall controller triggered only when signed in
    - This page can be seen only by student members and coordinators.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/controllers/viewall_controller.rb
+++ app/controllers/viewall_controller.rb
@@ -1,5 +1,11 @@
 class ViewallController < ApplicationController
   def index
+  	if not student_member_signed_in? and not coordinator_signed_in?
+
+  		render plain: "You don't have privileges to see this page."
+
+  	end
+
   	@allAlums = Alumni.all
   end
 
