dc311c4529eb691150f8752463aeb8f569738795
Hiten Chowdhary <hiten.iitkgp@gmail.com>
2015-05-14 21:57:19 +0530

    TODO lines removed

--- app/controllers/viewall_controller.rb
+++ app/controllers/viewall_controller.rb
@@ -10,7 +10,6 @@ class ViewallController < ApplicationController
   end
 
   def students
-  	## TODO: BY hiten
   	if not student_member_signed_in? and not coordinator_signed_in?
   		render plain: "You dont have privileges to see this page."
   	end
