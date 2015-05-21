9ab098b48ffbe5d9c0243d9c8a00b39e40a9270f
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-05-22 03:45:08 +0530

    Add TODO notes in the files.
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,7 +1,17 @@
 class BillsController < ApplicationController
   def index
+  	'''
+  	TODO by Hiten
+
+  	This action will show all the outstanding bills to the user.
+  	'''
   end
 
   def add
+  	'''
+  	TODO by Hiten
+
+  	This action, and its view will add a bill to the database.
+  	'''
   end
 end
