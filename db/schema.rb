e8e386a5d570df0f8efa1acb6f011f9ae87f1904
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-09 16:51:38 +0530

    Add migration and update schema
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521221220) do
+ActiveRecord::Schema.define(version: 20150609112107) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -57,6 +57,8 @@ ActiveRecord::Schema.define(version: 20150521221220) do
     t.string   "mobile_phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email1"
+    t.string   "email2"
   end
 
   add_index "alumni_data", ["Alumni_id"], name: "index_alumni_data_on_Alumni_id"
