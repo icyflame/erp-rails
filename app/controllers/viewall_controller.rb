0fd9a9a495bf46e24a111ea6d320a0152c046090
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-10 10:49:09 +0530

    Add the 403 response, and trigger it in controller
    403 Forbidden.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/controllers/viewall_controller.rb
+++ app/controllers/viewall_controller.rb
@@ -2,18 +2,22 @@ class ViewallController < ApplicationController
   def alumni
   	if not student_member_signed_in? and not coordinator_signed_in?
 
-  		render plain: "You don't have privileges to see this page."
+      render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
+      return
 
-  	end
 
-  	@allAlums = Alumni.all
+    end
+
+    @allAlums = Alumni.all
   end
 
   def students
   	if not student_member_signed_in? and not coordinator_signed_in?
-  		render plain: "You dont have privileges to see this page."
-  	end
+      render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
+      return
 
-  	@allStudents = StudentMember.all
+    end
+
+    @allStudents = StudentMember.all
   end
 end
