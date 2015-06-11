c241c27c0031869dac76df3f63492280c6050c60
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-11 21:43:07 +0530

    Add the new paid response in enum
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/models/alumni_status.rb
+++ app/models/alumni_status.rb
@@ -1,7 +1,7 @@
 class AlumniStatus < ActiveRecord::Base
 	belongs_to :Alumni
 	enum search: [:ready, :found, :tobesearched, :dumped]
-	enum response: [:positive, :negative, :neutral, :registered]
+	enum response: [:positive, :negative, :neutral, :registered, :paid]
 
 	after_create :init
 
