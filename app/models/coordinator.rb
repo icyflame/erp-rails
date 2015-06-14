c33f12bd30cc060d88d5e151e654c877f12f8ae2
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-14 13:39:31 +0530

    Make changes to the sending pattern
    - Send using the Mailgun Message Builder (instead of 
      writing custom JSON)
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -10,29 +10,25 @@ class Coordinator < ActiveRecord::Base
   def send_confirmation_mail
 
   	fin = File.open "public/mail-templates/welcome.html"
-
   	html_text_from_file = fin.read
-
   	fin.close
-
   	replacer = StringEnumerator.new(:type => ["Coordinator"], :name => [self.name])
-
   	html_text_from_file = replacer.enumerate html_text_from_file
 
-		require_relative './mailgun.rb'
+    mg_client = Mailgun::Client.new "key-74e2fcede7797b9b869f574c965c9b0d"
+    mb_obj = Mailgun::MessageBuilder.new
 
-		mg_client = Mailgun::Client.new "key-74e2fcede7797b9b869f574c965c9b0d"
-
-		message_params = {:from    => 'no-reply@erp.alumnicell.com',
-		                  :to      => self.email,
-		                  :subject => 'You have registered on the SAC ERP',
-		                  :text    => 'Hello, Just letting you know that your account has been created, Regards,\n\nSystem Administrator.',
-		                  :html    => html_text_from_file
-		                }
+    mb_obj.set_from_address("no-reply@erp.alumnicell.com", {"first"=>"SAC", "last" => "Internal ERP"});
+    mb_obj.add_recipient(:to, self.email);
+    mb_obj.set_subject("You have registered on the SAC ERP");
+    mb_obj.set_text_body("Hello, Just letting you know that your account has been created, Regards, System Administrator.");
+    mb_obj.set_html_body(html_text_from_file)
 
 		puts "Sending the email, now!"
 
-		mg_client.send_message "sandbox3a6bc1bbcfc149ba9e95376871456536.mailgun.org", message_params
+		mg_client.send_message("sandbox3a6bc1bbcfc149ba9e95376871456536.mailgun.org", mb_obj)
+
+    puts "Mail has been sent! :D"
 
   end
 end
