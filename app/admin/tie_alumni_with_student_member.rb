02a21bb33002eb141a7a733e747c4fdd5527c615
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-06-15 20:03:36 +0530

    Fix the admin files
    - Raises #62
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/admin/tie_alumni_with_student_member.rb
+++ app/admin/tie_alumni_with_student_member.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register TieAlumniWithStudentMember do
-  permit_params :alumni_id, :studentMember_id
+
+  # permit_params :alumni_id, :studentmember_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
