7526870fc4db68d646d72774781ea082a39baf03
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-08-18 01:07:00 +0530

    Add tests for the sponsors controller.
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,4 +11,24 @@ FactoryGirl.define do
       rollnum "13TH12345"
       password "ruby12345"
   end
+  factory :Sponsor do
+    company "some_string_value"
+    type_of_sponsorship "some_string_value"
+    person "some_string_value"
+    mobile_phone "some_string_value"
+    office_phone "some_string_value"
+    email1 "some_string_value"
+    email2 "some_string_value"
+    designation "some_string_value"
+    alumnus "some_string_value"
+    batch "some_string_value"
+    dept "some_string_value"
+    hall "some_string_value"
+    first_date "some_string_value"
+    comments "some_string_value"
+    next_date "some_string_value"
+    proposal_sent "some_string_value"
+    remarks "some_string_value"
+    status "some_string_value"
+  end
 end
