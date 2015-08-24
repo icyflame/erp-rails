67287f1374bfe2ebe6e8d1d018b88c2f797c4008
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-08-25 00:38:06 +0530

    [major] Bug Fix, tie record exists. only update.
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- app/controllers/allotment_controller.rb
+++ app/controllers/allotment_controller.rb
@@ -6,20 +6,19 @@ class AllotmentController < ApplicationController
     # alum_to_allot = (0..619).to_a
 
     num_to_allot_each = alum_to_allot.count / ids_to_allot.count
-    num_to_allot_each = 50
+    num_to_allot_each = 35
 
     iterator = 0
 
     alum_to_allot.each do |alumnus|
-      temp = TieAlumniWithStudentMember.new
+      temp = TieAlumniWithStudentMember.where(:alumni_id => alumus.id).first
       # temp.alumni_id        = alumnus.id.to_i
-      temp.alumni_id        = alumnus
       studentmember_index = (iterator / num_to_allot_each)
       if studentmember_index >= ids_to_allot.count
         puts 'Overflow!'
       else
         temp.studentmember_id = ids_to_allot[studentmember_index]
-        puts temp.alumni_id.to_s + ' alumnus allotted to student member ' + temp.studentmember_id.to_s + ' ' + Student.find(temp.studentmember_id).name
+        puts temp.alumni_id.to_s + ' alumnus allotted to student member ' + temp.studentmember_id.to_s
         # temp.save!
       end
       iterator += 1
